b5ae4b3ce5239513780dd9593da940867121bdbb
dhvanilp <dhvanilhparikh@gmail.com>
2019-08-16 22:34:52 +0530

    minor changes mafe

--- truffle-config.js
+++ truffle-config.js
@@ -3,12 +3,17 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id
     },
     develop: {
       port: 8545
+    },
+    azureNet: {
+      host: "ethqx3k4a-dns-reg1.eastus.cloudapp.azure.com",
+      port: 8540,
+      network_id: "10101010" // Match any network id
     }
   }
 };
